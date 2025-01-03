685b476ef89073ee7e22ffb88bc2fdeaf5345f19
Vishnu-Selva-Kumar <vis.selva.git@gmail.com>
2025-01-03 22:42:51 +0530

    add package model init

---
+++ database/migrations/2025_01_03_223454_create_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique()->nullable();
+            $table->foreignIdFor(Category::class)->nullable();
+            $table->text('image')->nullable();
+            $table->text('short_descrtiption')->nullable();
+            $table->text('descrtiption')->nullable();
+            $table->integer('status')->default(config('web.constants.status.active'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};
