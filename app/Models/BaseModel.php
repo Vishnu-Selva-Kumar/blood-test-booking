ef3f6006439494ce79697c184751bf551da23554
Vishnu-Selva-Kumar <vis.selva.git@gmail.com>
2025-01-03 22:07:31 +0530

    base model init

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    //
+
+    protected $guarded = [];
+
+    protected $perPage = 30;
+
+    public function scopeSearch($query, $search)
+    {
+        if ($search) {
+            return $query->where('name', 'like', '%' . $search . '%');
+        }
+        return $query;
+    }
+
+    public function scopeSort($query, $sort)
+    {
+        if ($sort) {
+            return $query->orderBy($sort);
+        }
+        return $query;
+    }
+
+    public function getstatusTextAttribute()
+    {
+        return $this->status ? 'Active' : "In Active";
+    }
+
+
+    public function scopeWhereInActive($query)
+    {
+        return $query->where('status', config('constants.status.inactive'));
+    }
+
+    public function scopeWhereActive($query)
+    {
+        return $query->where('status', config('constants.status.active'));
+    }
+}
