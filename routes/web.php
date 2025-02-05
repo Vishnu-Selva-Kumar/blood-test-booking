9c83ab8fd014025f3bb832a9ddfbd050ceb46c20
Vishnu-Selva-Kumar <vis.selva.git@gmail.com>
2025-02-05 22:07:26 +0530

    import theme asset file and home page create

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('web.')->group(function () {
+
+    Route::view('/', 'home')->name('home');
+
 });
