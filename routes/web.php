e8a146161528fca9933086cc10b6658cf49d4e8e
Vishnu-Selva-Kumar <vis.selva.git@gmail.com>
2025-02-18 13:21:57 +0530

    import pages

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,7 @@ Route::name('web.')->group(function () {
     Route::view('/portfolio-details', 'portfolio-details')->name('portfolio-details');
     Route::view('/doctors', 'doctors')->name('doctors');
     Route::view('/blog-detail', 'blog-details')->name('blog-details');
+    Route::view('/doctor-detail', 'doctor-details')->name('doctor-details');
+    Route::view('/service', 'service')->name('service');
+    Route::view('/service-details', 'service-details')->name('service-details');
 });
